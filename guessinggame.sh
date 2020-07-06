d6b990638409acc839958cc5c755a092e3507d00
Prangon Ghose <prangon@enertiv.com>
2020-07-05 20:21:01 -0400

    automated number of files calculation

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,28 +2,27 @@
 
 echo "--Program Start--"
 
-echo "Type in the number of files in the directory and then press Enter:"
-read response
-
+num_files=$(ls -a | wc -l)
 guess=-1
 
 function guessinggame {
-    while [[ $guess -ne $response ]]
+    while [[ $guess -ne $num_files ]]
     do
         echo "Type in your guess for the number of files in the directory and then press Enter:"
         read guess
 
-        if [[ $guess -gt $response ]]
+        if [[ $guess -gt $num_files ]]
         then
             echo "You entered $guess. $guess is too high. Please try again."
-        elif [[ $guess -lt $response ]]
+        elif [[ $guess -lt $num_files ]]
         then
             echo "You entered $guess. $guess is too low. Please try again."
-        elif [[ $guess -eq $response ]]
+        elif [[ $guess -eq $num_files ]]
         then
             echo "Congratulations! $guess is the correct answer."
         else
-            echo "You entered $guess. This is an invalid guess. Please try again."
+            echo "You entered $guess. This is an invalid guess. Please try again with a positive integer value."
+            let guess=-1
         fi
     echo
     done
